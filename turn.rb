aa9bfe94012a74ea6fbac3ed6a74dd8dd0d3497f
Devine Lu Linvega <aliceffekt@gmail.com>
2016-01-30 16:52:34 -0800

    First draft at a code parser

--- turn.rb
+++ turn.rb
@@ -1,36 +1,94 @@
-
 class Blindfolk
 
 	def initialize id,code
-		@code = code
-		actionsLeft = 10
+		@id = id
+		@rules = parse(code)
+		stamina = 10
+		@actionIndex = 0
+		@status = "default"
+	end
+
+	def parse code
+
+		rules = {}
+		code = code.gsub("\n\n","\n")
+		_case = "default"
+		code.lines.each do |line|
+			line = line.strip
+			if line == "" then next end
+			if line[0,5] == "case " then _case = line.sub("case ","") ; next end
+			if !rules[_case] then rules[_case] = [] end
+			rules[_case].push(line)
+		end
+		return rules
+
+	end
+
+	def handlers
+
+	end
+
+	def status
+
+		return "default"
+
+	end
+
+	def act phase
+
+		actionIndexClamped = @actionIndex % @rules[@status].length
+		puts "#{@id} [#{status}] -> #{phase} * #{@rules[@status][actionIndexClamped]}"
+		@actionIndex += 1
+	end
+
+	def action
+		return @code
 	end
 
 end
 
+# Create Players
+
 # Make Player1
 code = "
-case default
-  move.forward
+move.forward
+move.backward
 "
 p1 = Blindfolk.new(1,code)
 
 # Make Player2
 code = "
-case default
-  move.forward
+turn.right
+turn.right
 "
 p2 = Blindfolk.new(2,code)
 
-players = [p1,p2]
+# Make Player3
+code = "
+case hit.high
+  attack.high
+  turn.left
+case default
+  attack.high
+  turn.left
+"
+p3 = Blindfolk.new(3,code)
 
-puts "Start!"
+players = [p1,p2,p3].shuffle
 
+# Play
 
+phase = 1
+while phase <= 5
+	puts "PHASE #{phase}"
+	for player in players
+		player.act(phase)
+	end
+	phase += 1
+end
 
-
-
-
+puts "SUMMARY"
+puts "[missing]"
 
 
 
