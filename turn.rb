372f0d8c0798912aa919d8520bab9c9e61851813
Devine Lu Linvega <aliceffekt@gmail.com>
2016-01-30 22:09:34 -0800

    Cleaned the turn.rb file

--- turn.rb
+++ turn.rb
@@ -1,5 +1,7 @@
 require_relative "class.blindfolk.rb"
 
+$logs = {}
+
 def log event
 
 	if !$logs[$phase] then $logs[$phase] = [] end
@@ -15,6 +17,42 @@ def playersAlive
 	end
 
 	return alive
+
+end
+
+def runPhase
+
+	$phase = 1
+	while $phase <= 10
+		log("# Phase: #{$phase}\n")
+		for player in $players
+			player.act()
+		end
+		if playersAlive == 1 then break end
+		$phase += 1
+	end
+	log("# End\n")
+
+	# Save scores
+	for player in $players
+		log("#{player.name}, score: #{player.score}")
+	end
+
+	# Last blindfolk standing
+	if playersAlive == 1
+		$players.each do |player|
+			if player.isAlive == 0 then next end
+			log("#{player.name} is the last one alive.")
+		end
+	end
+
+	# Print Logs
+	$logs.each do |phase,events|
+		if events.length == 0 then next end
+		events.each do |event|
+			puts "  #{event}"
+		end
+	end
 
 end
 
@@ -43,35 +81,8 @@ p3 = Blindfolk.new(3,0,0,code)
 
 $players = [p1,p3].shuffle
 
-$logs = {}
-
-# Play (TODO: Don't play less than 2 players are alive)
-
-$phase = 1
-while $phase <= 10
-	log("# Phase: #{$phase}\n")
-	for player in $players
-		player.act()
-	end
-	if playersAlive == 1 then break end
-	$phase += 1
-end
-
-log("# End\n")
-
-# Save scores
-
-for player in $players
-	log("#{player.name}, score: #{player.score}")
-end
-
-# Print Logs
-
-$logs.each do |phase,events|
-	if events.length == 0 then next end
-	events.each do |event|
-		puts "  #{event}"
-	end
+if playersAlive > 1
+	runPhase()
 end
 
 
