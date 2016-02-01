cf44c83fca0d8e8b5782997105bf6f319fb2002b
Devine Lu Linvega <aliceffekt@gmail.com>
2016-02-01 11:36:59 -0800

    Limit leaderboard to 10 players

--- inc/script.js
+++ inc/script.js
@@ -143,10 +143,13 @@ function loadLeaderboard()
 		$('#players_alive').html(leaderboard.playersCount.alive+"/"+leaderboard.playersCount.total+" Players");
 
 		var leaderboardText = "<span class='sh_rank'></span> <span class='sh_spacer'>|</span> <span class='sh_name'>Name</span> <span class='sh_spacer'>|</span> <span class='sh_score'>Score</span>\n";
+		var count = 0;
 		$.each(leaderboard.players, function( index, value ) {
 			if( parseInt(value[2]) > 0 ){
 				leaderboardText += "<span class='sh_rank'>"+value[0]+"</span> <span class='sh_spacer'>|</span> <span class='sh_name'>Blindfolk #"+value[1]+"</span> <span class='sh_spacer'>|</span> <span class='sh_score'>"+value[2]+" kills</span> <span class='sh_spacer'>|</span> <span>"+(parseInt(value[3]) == 1 ? "Alive" : "")+"</span>\n";
 			}
+			if(count> 10){ return false; }
+			count++;
 		});
 		$('#leaderboard').html(leaderboard.header+leaderboardText);
 
